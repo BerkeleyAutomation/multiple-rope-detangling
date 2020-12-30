cc3c659e224c199146214bedddfe6ccf506b4df4
Vainavi <vainavi@jensen.ist.berkeley.edu>
2020-12-29 17:15:10 -0800

    trying 4 channel

--- keypoints_cls/analysis.py
+++ keypoints_cls/analysis.py
@@ -15,20 +15,16 @@ import numpy as np
 
 # model
 keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH)
-#keypoints.load_state_dict(torch.load('checkpoints/dr_braid_varied/model_2_1_5_0.0026437892680103254.pth'))
-keypoints.load_state_dict(torch.load('checkpoints/black_out_simple_GAUSS_KPTS_ONLY/model_2_1_6_0.003018017431239951.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_termGAUSS_KPTS_ONLY/model_2_1_4_0.003552900240372758.pth'))
-
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term/model_2_1_13.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term/model_2_1_14.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term_braid_GAUSS_KPTS_ONLY/model_2_1_4_0.003553322965173738.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term_capsule_GAUSS_KPTS_ONLY/model_2_1_4_0.0032973564452098247.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/dr_cable_cycles_6400_GAUSS_KPTS_ONLY/model_2_1_10_0.0029300788630979243.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/dr_cable_cycles_9K_GAUSS_KPTS_ONLY/model_2_1_18_0.003089638756832411.pth'))
+keypoints.load_state_dict(torch.load('checkpoints/real_crop/model_2_1_24.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/cable_mask_dset_GAUSS_KPTS_ONLY/model_2_1_8_0.0033715498981842476.pth'))
 
 # cuda
 use_cuda = torch.cuda.is_available()
 #use_cuda = False
 if use_cuda:
-    torch.cuda.set_device(7)
+    torch.cuda.set_device(0)
     keypoints = keypoints.cuda()
 
 prediction = Prediction(keypoints, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, use_cuda)
@@ -36,16 +32,14 @@ transform = transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-#image_dir = 'data/global_cable/images'
-#image_dir = 'data/undo_reid_term_braid/test/images'
-#image_dir = 'data/undo_reid_term_capsule/test/images'
-image_dir = 'data/black_out_simple/test/blacked_out'
-#image_dir = 'data/real_braid_1'
+#image_dir = 'data/overhead_hairtie_random_resized_larger'
+image_dir = 'datasets/real_crop_test/images'
+#image_dir = 'data/hairtie_overcrossing_resized_masks'
+#image_dir = 'data/overhead_hairtie_resized_masks'
 classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
 for i, f in enumerate(sorted(os.listdir(image_dir))):
-    img = cv2.imread(os.path.join(image_dir, f))
-    #dim = (640,480)
-    #img = cv2.resize(img,dim) 
+    img = np.load(os.path.join(image_dir, f), allow_pickle=True)
+    print(img.shape)
     img_t = transform(img)
     img_t = img_t.cuda()
     # GAUSS
--- keypoints_cls/train.py
+++ keypoints_cls/train.py
@@ -13,7 +13,7 @@ from src.dataset import KeypointsDataset, transform
 MSE = torch.nn.MSELoss()
 bceLoss = nn.BCELoss
 
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 def forward(sample_batched, model):
     img, gt_gauss = sample_batched
@@ -45,29 +45,29 @@ def fit(train_data, test_data, model, epochs, checkpoint_path = ''):
             test_loss += loss.item()
         print('test loss:', test_loss / i_batch)
         if epoch%2 == 0:
-            torch.save(keypoints.state_dict(), checkpoint_path + '/model_2_1_' + str(epoch) + '_' + str(test_loss/i_batch) + '.pth')
+            torch.save(keypoints.state_dict(), checkpoint_path + '/model_2_1_' + str(epoch) + '.pth')
 
 # dataset
 workers=0
-dataset_dir = 'black_out_simple'
+dataset_dir = 'real_crop'
 output_dir = 'checkpoints'
-save_dir = os.path.join(output_dir, dataset_dir+'_GAUSS_KPTS_ONLY')
+save_dir = os.path.join(output_dir, dataset_dir)
 
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 
-train_dataset = KeypointsDataset('data/%s/train/blacked_out'%dataset_dir,
-                           'data/%s/train/keypoints'%dataset_dir, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, transform, gauss_sigma=GAUSS_SIGMA)
+train_dataset = KeypointsDataset('datasets/%s/train/images'%dataset_dir,
+                           'datasets/%s/train/annots'%dataset_dir, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, transform, gauss_sigma=GAUSS_SIGMA)
 train_data = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
 
-test_dataset = KeypointsDataset('data/%s/test/blacked_out'%dataset_dir,
-                           'data/%s/test/keypoints'%dataset_dir, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, transform, gauss_sigma=GAUSS_SIGMA)
+test_dataset = KeypointsDataset('datasets/%s/test/images'%dataset_dir,
+                           'datasets/%s/test/annots'%dataset_dir, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, transform, gauss_sigma=GAUSS_SIGMA)
 test_data = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
 
 use_cuda = torch.cuda.is_available()
-
+print("USE CUDA", use_cuda)
 Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 if use_cuda:
     torch.cuda.set_device(0)
@@ -76,7 +76,7 @@ if use_cuda:
 keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH).cuda()
 
 # optimizer
-#optimizer = optim.Adam(keypoints.parameters(), lr=1.0e-4, weight_decay=1.0e-4)
-optimizer = optim.Adam(keypoints.parameters(), lr=0.0001)
+optimizer = optim.Adam(keypoints.parameters(), lr=1.0e-4, weight_decay=1.0e-4)
+#optimizer = optim.Adam(keypoints.parameters(), lr=0.0001)
 
 fit(train_data, test_data, keypoints, epochs=epochs, checkpoint_path=save_dir)
