9a7803c02a3bac69ee9e5f863fa2554b6de67ee3
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-01-06 12:02:11 -0800

    stuff

--- keypoints_cls/analysis.py
+++ keypoints_cls/analysis.py
@@ -13,17 +13,21 @@ from datetime import datetime
 from PIL import Image
 import numpy as np
 
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
+
 # model
 keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH)
 #keypoints.load_state_dict(torch.load('checkpoints/dr_braid_varied/model_2_1_5_0.0026437892680103254.pth'))
-keypoints.load_state_dict(torch.load('checkpoints/two_hairties_ep_home_GAUSS_KPTS_ONLY/model_2_1_4_0.0032886856630272823.pth'))
+keypoints.load_state_dict(torch.load('checkpoints/two_hairties_ep_home_3_GAUSS_KPTS_ONLY/model_2_1_4_0.0028477227005818864.pth'))
 #keypoints.load_state_dict(torch.load('checkpoints/undo_reid_termGAUSS_KPTS_ONLY/model_2_1_4_0.003552900240372758.pth'))
 
 # cuda
 use_cuda = torch.cuda.is_available()
+print(use_cuda)
 #use_cuda = False
 if use_cuda:
-    torch.cuda.set_device(7)
+    print("inside")
+    torch.cuda.set_device(0)
     keypoints = keypoints.cuda()
 
 prediction = Prediction(keypoints, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, use_cuda)
@@ -34,7 +38,7 @@ transform = transform = transforms.Compose([
 #image_dir = 'data/global_cable/images'
 #image_dir = 'data/undo_reid_term_braid/test/images'
 #image_dir = 'data/undo_reid_term_capsule/test/images'
-image_dir = 'data/train_sets/two_hairties_pp_home/test/images'
+image_dir = 'data/train_sets/two_hairties_pp_home_4/test/images'
 #image_dir = 'data/real_braid_1'
 classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
 for i, f in enumerate(sorted(os.listdir(image_dir))):
