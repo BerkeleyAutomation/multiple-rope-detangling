387c83334ee8f4c3a1a1b4c0e17add90b2362a1e
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-01-14 13:36:35 -0800

    stuff

--- keypoints_cls/src/prediction.py
+++ keypoints_cls/src/prediction.py
@@ -72,24 +72,19 @@ class Prediction:
         max_x = 0
         min_y = 480
         heat = heatmap[0][0]
-        #i = np.random.randint(0, high=self.num_keypoints)
-        for i in range(self.num_keypoints):
-            h = heatmap[0][i]
-            tmp = self.expectation(h)
-            pred_y, pred_x = np.unravel_index(h.argmax(), h.shape)
-            if pred_x > max_x and pred_y < min_y:
-                max_x = pred_x
-                min_y = pred_y 
-                heat = h
+        i = np.random.randint(0, high=self.num_keypoints)
+        #for i in range(self.num_keypoints):
+        h = heatmap[0][i]
+        tmp = self.expectation(h)
+        pred_y, pred_x = np.unravel_index(h.argmax(), h.shape)
+            #if pred_x > max_x and pred_y < min_y:
+            #    max_x = pred_x
+            #    min_y = pred_y 
+            #    heat = h
         vis = cv2.normalize(heat, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
         vis = cv2.applyColorMap(vis, cv2.COLORMAP_JET)
         result = cv2.addWeighted(img, 0.65, vis, 0.35, 0)
         result = cv2.circle(result, (max_x,min_y), 4, (0,0,0), -1)
-        #all_overlays.append(overlay)
-        #result1 = cv2.vconcat(all_overlays[:self.num_keypoints//2])
-        #result2 = cv2.vconcat(all_overlays[self.num_keypoints//2:])
-        #result = cv2.hconcat((result1, result2))
-        #cv2.putText(result)
         if cls is not None:
             label = classes[cls]
             cv2.putText(result, label, (10, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
