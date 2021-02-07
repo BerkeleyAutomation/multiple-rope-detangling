7203ccec9fc0986b6429ee1388a1d3362f68e153
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-02-07 13:55:05 -0800

    stuff

--- keypoints_cls/analysis.py
+++ keypoints_cls/analysis.py
@@ -33,7 +33,7 @@ transform = transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-image_dir = 'data/real_images/two_rope_test_crop'
+image_dir = 'data/real_images/two_rope_test_crop2'
 classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
 for i, f in enumerate(sorted(os.listdir(image_dir))):
     img = cv2.imread(os.path.join(image_dir, f))
