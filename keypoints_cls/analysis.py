f9ca4a3c440837b61116066e2bffffcf4de5ee7d
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-02-08 21:50:04 -0800

    stuff

--- keypoints_cls/analysis.py
+++ keypoints_cls/analysis.py
@@ -17,7 +17,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 # model
 keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH)
-keypoints.load_state_dict(torch.load('checkpoints/reid_two_rope/model_2_1_2_0.0035196005673935327.pth'))
+keypoints.load_state_dict(torch.load('checkpoints/reid_zoom/model_2_1_2_0.0033394121974786133.pth'))
 
 # cuda
 use_cuda = torch.cuda.is_available()
@@ -33,7 +33,7 @@ transform = transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-image_dir = 'data/real_images/two_rope_term30_crop'
+image_dir = 'data/train_sets/reid_zoom/test/images'
 classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
 for i, f in enumerate(sorted(os.listdir(image_dir))):
     img = cv2.imread(os.path.join(image_dir, f))
