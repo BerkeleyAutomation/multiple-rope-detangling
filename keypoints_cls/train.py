59aa3dacabe03337f63c2410d0e6625badb02bfc
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-02-21 18:06:55 -0800

    stuff

--- keypoints_cls/train.py
+++ keypoints_cls/train.py
@@ -50,7 +50,7 @@ def fit(train_data, test_data, model, epochs, checkpoint_path = ''):
 # dataset
 workers=0
 raid_dir = 'train_sets'
-dir_name = 'reid_no_color'
+dir_name = 'two_rope_hulk'
 dataset_dir = raid_dir + '/' + dir_name
 output_dir = 'checkpoints'
 save_dir = os.path.join(output_dir, dir_name)
