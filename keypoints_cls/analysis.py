cf87d5765e86c687b5899b7d90a60a1eefaf787e
Vainavi <vainavi@jensen.ist.berkeley.edu>
2021-02-28 20:14:01 -0800

    stuff

--- keypoints_cls/analysis.py
+++ keypoints_cls/analysis.py
@@ -18,7 +18,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="2"
 # model
 keypoints = Resnet4Channel(num_classes=1, num_channels=4, pretrained=False).cuda()
 #keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH)
-keypoints.load_state_dict(torch.load('checkpoints/term_zoom_cond/model_2_1_16_0.0038759687399295803.pth'))
+keypoints.load_state_dict(torch.load('checkpoints/terminate/model_2_1_22_0.08286855734001751.pth'))
 
 # cuda
 use_cuda = torch.cuda.is_available()
@@ -32,7 +32,7 @@ transform = transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-data_dir = "real_images/term_test_imgs"
+data_dir = "train_sets/terminate/test"
 test_dataset = KeypointsDataset('data/%s/images'%data_dir,
                            'data/%s/annots'%data_dir, NUM_KEYPOINTS, IMG_HEIGHT, IMG_WIDTH, transform, gauss_sigma=GAUSS_SIGMA)
 test_data = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=0)
